b812e09a54437f0402ad943aaf3aa99a911896f4
per-pj <189612052+per-pj@users.noreply.github.com>
2025-03-22 05:38:09 +0900

    headerとfooterを追加、背景色やテキストのデザイン調整

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
-// const API_URL = '/.netlify/functions/fetchData'; // Netlify Functionのエンドポイント
+const toggleMenu = document.querySelector('.toggle-menu');
+const nav = document.querySelector('.nav');
+const navLinks = document.querySelectorAll('.nav-menu a');
+
+if (toggleMenu && nav && navLinks) {
+  toggleMenu.addEventListener('click', () => {
+    toggleMenu.classList.toggle('open'); // トグルメニューのopenクラスを切り替え
+    nav.classList.toggle('open');
+  });
+
+  navLinks.forEach((link) => {
+    link.addEventListener('click', () => {
+      toggleMenu.classList.remove('open');
+      nav.classList.remove('open');
+    });
+  });
+}
 
 let API_URL;
 
